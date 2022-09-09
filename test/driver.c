87c0fcea01f3df95214f24d4c9f1d5328fa0e71c
Allen <22706998@student.uwa.edu.au>
2022-09-09 18:17:28 +0800

    update to write errors to logfile

--- test/driver.c
+++ test/driver.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 #include <time.h>
 
 #define N_MIN 100
@@ -15,39 +17,61 @@
 #define P_RES (int)1e3
 
 #define NT_MIN 1
-#define NT_MAX 4
+#define NT_MAX 12
 #define NT_STEP 1
 
 #define ARG_LENGTH 40
 
-#define RESULTS_FILE "../test/results.csv"
+#define RESULTS_FILE "results.csv"
+
+extern int errno ;
 
 /**
- * USAGE: ./driver [RESULTS_FILENAME]
+ * USAGE: ./driver [-f RESULTS_FILENAME] [-l LOG_FILENAME]
  * 
  * Loops through n, p and n_threads values and calls percolate program infinitely, which appends results to file.
  */
 int main(int argc, char *argv[]) {
   srand(time(NULL));
-  short err = 0;
+  int opt;
+  char* resfile_name = NULL;
+  FILE *logfile = NULL;
+
+  while((opt = getopt(argc, argv, "f:l:")) != -1) {
+    switch(opt) {
+      case 'f':
+        resfile_name = optarg;
+        break;
+      case 'l':
+        logfile = fopen(optarg, "a");
+        break;
+      default:
+        fprintf(stderr, "Usage: [-f RESULTS_FILENAME] [-l LOG_FILENAME]");
+    }
+  }
+
   while(1) {
     for(int n = N_MIN; n <= N_MAX; n+=N_STEP) {
-      if(err) break;
       for(int pi = (int)(P_MIN*P_RES); pi <= (int)(P_MAX*P_RES); pi+=(int)(P_STEP*P_RES)) {
         float p = (float)pi/P_RES;
         int seed = rand(); // same seed for each n_threads to make sure the results are identical
-        if(err) break;
         for(int nt = NT_MIN; nt <= NT_MAX; nt+=NT_STEP) {
           int pid = fork();
+
           if(pid == -1) {
-            err = 1;
+            char* errstr = strerror(errno);
+            fprintf(stderr, "Error: %s", errstr);
+            if (logfile != NULL) {
+              fprintf(logfile, "Error: %s with parameters N=%i, P=%f, NT=%i\n", errstr, n, p, nt);
+            }
             break;
           }
+
           else if(pid == 0) { // child
             char *args[] = {"percolate", "-v", "-p", NULL, "-r", NULL, NULL, NULL, NULL, NULL};
             args[3] = malloc(ARG_LENGTH*sizeof(char));
             for(int i = 5; i < 9; ++i) args[i] = malloc(ARG_LENGTH*sizeof(char));
-            sprintf(args[3], "%s", argc > 1 ? argv[1] : RESULTS_FILE);
+            sprintf(args[3], "%s", resfile_name != NULL ? resfile_name : RESULTS_FILE);
             sprintf(args[5], "%d", seed); // same seed for all n_threads
             sprintf(args[6], "%d", n);
             sprintf(args[7], "%f", p);
@@ -55,6 +79,7 @@ int main(int argc, char *argv[]) {
             execv("../src/percolate", args);
             exit(EXIT_FAILURE);
           }
+
           else {
             int status;
             wait(&status); // wait for child
